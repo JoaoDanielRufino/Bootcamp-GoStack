334e7c3c0da734b06957eeb0adcbdfd56742b1ee
Joao Daniel Rufino <joaodaniel@Joaos-MacBook-Pro.local>
2019-08-20 23:51:32 -0300

    ...

---
+++ gobarber-backend/src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import User from './app/models/User';
+
+const routes = new Router();
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Joao Daniel',
+    email: 'joaodaniel@gmail.com',
+    password_hash: '123456'
+  });
+
+  return res.json(user);
+});
+
+export default routes;
