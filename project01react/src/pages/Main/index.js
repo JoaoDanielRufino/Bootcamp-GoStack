4b9d217e3f2d6a728e21b2706aad387915512122
JoaoDanielRufino <joaodaniel0405@gmail.com>
2019-08-29 22:53:39 -0300

    ReactJS

---
+++ project01react/src/pages/Main/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
+
+function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositorios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositorio" />
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
+
+export default Main;
