d2832c5e934b29461e2eeaa68a70236829367ec0
JoaoDanielRufino <joaodaniel0405@gmail.com>
2020-03-26 19:14:25 -0300

    ...

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,7 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
+
+// O redux saga serve para adicionar um middleware entre uma action e
+// um reducer, assim eh possivel modificar os dados que uma action
+// esta enviando, e o reducer receber os dados necessarios ou 
+// realizar alguma regra de negocio
 
 export default store;
