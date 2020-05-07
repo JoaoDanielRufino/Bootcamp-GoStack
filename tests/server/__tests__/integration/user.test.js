e83b2da161a5f09616a5a45bef7e88a77c43881e
JoaoDanielRufino <joaodaniel0405@gmail.com>
2020-05-07 17:26:52 -0300

    ...

---
+++ tests/server/__tests__/integration/user.test.js
@@ -0,0 +1,44 @@
+import request from 'supertest';
+import bcrypt from 'bcryptjs';
+import app from '../../src/app';
+
+import factory from '../factories';
+import truncate from '../util/truncate';
+
+describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await factory.create('User', { password: '1234' });
+
+    const compareHash = await bcrypt.compare('1234', user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+
+  it('should be able to register', async () => {
+    const user = await factory.attrs('User');
+
+    const response = await request(app)
+      .post('/users')
+      .send(user);
+
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to registerwith duplicated email', async () => {
+    const user = await factory.attrs('User');
+
+    await request(app)
+      .post('/users')
+      .send(user);
+
+    const response = await request(app)
+      .post('/users')
+      .send(user);
+
+    expect(response.status).toBe(400);
+  });
+});
