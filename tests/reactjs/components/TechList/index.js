df8bfd1803c5e217ee7993bd8d190674153220bf
JoaoDanielRufino <joaodaniel0405@gmail.com>
2020-05-11 15:24:27 -0300

    ...

---
+++ tests/reactjs/components/TechList/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+export default function TechList() {
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAddTech() {
+    setTechs([...techs, 'Node.js']);
+    setNewTech('');
+  }
+
+  return (
+    <form data-testid="tech-form" onSubmit={handleAddTech}>
+      <ul data-testid="tech-list">
+        {techs.map(tech => <li key={tech}>{tech}</li>)}
+      </ul>
+
+      <label htmlFor="tech">Tech</label>
+      <input id="tech" value={newTech} onChange={e => setNewTech(e.target.value)} />
+
+      <button onClick={handleAddTech}>Adicionar</button>
+    </form>
+  );
+}
